d6668ea1e44e1fcb7a0845dcfe15723cea0a2828
BrookLiNN <106794039+AllanPauluk@users.noreply.github.com>
2022-10-20 01:02:41 +0000

    Criando linha

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-'''
-Comentario em bloco
+#Projeto Python Curso de Extensão
 
+'''
+Comentário em bloco
 '''
 
 print("Hello Word!")
 
+#Não é necessário declarar o tipo de variável
+nome = "Allan Medeiros"
+idade = 18
+
+print("Meu nome é " + nome, "\nE eu tenho " + str(idade), "anos")
