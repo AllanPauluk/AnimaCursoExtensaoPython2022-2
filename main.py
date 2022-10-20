d57e83485d5c2ac3b7ee72cea6c023aa942a4099
BrookLiNN <106794039+AllanPauluk@users.noreply.github.com>
2022-10-20 01:19:44 +0000

    Linhas de códigos

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ print("Hello Word!")
 nome = "Allan Medeiros"
 idade = 18
 
+#print(f"Meu nome é " + nome, "\nE eu tenho " + {idade}, "anos")
+#print("Meu nome é {} anos".format(idade))
+#print("Meu nome é {} \nE eu tenho {} anos".format(nome, idade))
 print("Meu nome é " + nome, "\nE eu tenho " + str(idade), "anos")
+#print("meu nome é {0}, e eu tenho {1}".format(nome, idade)) 
